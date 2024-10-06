f266a99d03295f7b51d93de1cd6eef70d065d1dd
Matimoo007 <mathieuvezina7@gmail.com>
2024-10-06 16:33:50 -0400

    game over

--- ludumdare/Scripts/game_over.gd
+++ ludumdare/Scripts/game_over.gd
@@ -1,10 +1,7 @@
 extends Control
 
-func _on_game_over() -> void:
-	visible = true
-
 func _on_restart_pressed() -> void:
-	get_tree().reload_current_scene()
+	get_tree().change_scene_to_file("res://Scenes/World.tscn")
 
 func _on_main_screen_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu/MainMenu.tscn")
